c910ff54818c5bab57028c8c8469ed6042c6222e
יהודית ליפשיץ <ylifshit@g.jct.ac.il>
2020-10-17 23:48:48 +0300

    +welcome2389

---
+++ dotNet5781_4484_2389/dotNet5781_00_4484_2389/Program2389.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_4484_2389
+{
+   partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome2389();
+            Welcome4484();
+            Console.ReadKey();
+        }
+
+       static partial void Welcome4484();
+        private static void Welcome2389()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine(name + ", welcome to my first console application");
+        }
+    }
+}

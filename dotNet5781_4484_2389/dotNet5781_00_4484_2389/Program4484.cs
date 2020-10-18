60414a77c4e4d62eb270c86e57538e5d6bb7bfe2
שירה <shorbach@g.jct.ac.il>
2020-10-18 16:32:34 +0300

    +welcome4484

---
+++ dotNet5781_4484_2389/dotNet5781_00_4484_2389/Program4484.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_4484_2389
+{
+    partial class Program
+    {
+        static partial void Welcome4484()
+        {
+            Console.WriteLine("Hey there! I am using VS too");
+        }
+    }
+}

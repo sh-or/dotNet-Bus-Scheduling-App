1c3c1a15c85b71d59a3b73db91540cb9170d5d5b
שירה <shorbach@g.jct.ac.il>
2020-10-27 16:32:35 +0200

    f try

---
+++ dotNet5781_4484_2389/dotNet5781_01_4484_2389/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_01_4484_2389
+{
+    class Program
+    {
+        static void Welcome()
+        {
+            Console.WriteLine("Hey there! I am using VS too");
+        }
+        static void Main(string[] args)
+        {
+            Welcome();
+        }
+    }
+}

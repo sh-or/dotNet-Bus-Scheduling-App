3e45a46ac76206c4c71016b4ac5ac0acf5adcfe4
יהודית ליפשיץ <ylifshit@g.jct.ac.il>
2020-11-10 19:24:13 +0200

    ienumerable

--- dotNet5781_4484_2389/dotNet5781_02_4484_2389/BusLineSystem.cs
+++ dotNet5781_4484_2389/dotNet5781_02_4484_2389/BusLineSystem.cs
@@ -1,12 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace dotNet5781_02_4484_2389
 {
-    class BusLineSystem
+    class BusLineSystem /*: IEnumerable*/
     {
+
+        List<BusLine> busLinesList;
+
+        BusLineSystem()
+        {
+            busLinesList = new List<BusLine>();
+        }
+
+        //public IEnumerator GetEnumerator()
+        //{
+        //    //foreach (var item in busLinesList)
+        //    //{
+        //    //    yield return item;
+        //    //}
+        //    return busLinesList.GetEnumerator();
+        //}
+
+        public interface IEnumerator
+        {
+            object Current { get; }
+            bool MoveNext();
+            void Reset();
+        }
+
+        public interface IEnumerable
+        {
+            IEnumerator GetEnumerator();
+        }
     }
+
 }
+
+
+}
+
+
+}
+

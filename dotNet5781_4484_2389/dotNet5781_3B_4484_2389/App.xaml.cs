af5c622fbe9f0df8511c3bbf0f3d037cec816ea8
שירה <shorbach@g.jct.ac.il>
2020-11-24 19:17:02 +0200

    restart

---
+++ dotNet5781_4484_2389/dotNet5781_3B_4484_2389/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace dotNet5781_3B_4484_2389
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}

e32224540160282a7b086f029e942caec5212cc4
שירה <shorbach@g.jct.ac.il>
2021-01-12 17:27:15 +0200

    user win

--- dotNet5781_4484_2389/PL/User.xaml.cs
+++ dotNet5781_4484_2389/PL/User.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using BO;
+using BlAPI;
 
 namespace PL
 {
@@ -19,9 +21,82 @@ namespace PL
     /// </summary>
     public partial class User : Window
     {
-        public User()
+        IBL bl;
+
+        public User(IBL ibl)
         {
+            bl = ibl;
             InitializeComponent();
+            //IEnumerable<BOBusStation> busStations = bl.GetAllBusStations();
+            ListBusStation.ItemsSource = bl.GetAllBusStations();
+            //IEnumerable<BOLine> line = bl.GetAllLines();
+            ListLines.ItemsSource = bl.GetAllLines();
+            StationLines.ItemsSource = (ListBusStation.SelectedItem as BOBusStation).Lines;
+            LineStations.ItemsSource = (ListLines.SelectedItem as BOLine).Stations;
         }
+
+        private void RefreshLinesAndStations(object sender, EventArgs e)
+        {
+            ListBusStation.ItemsSource = bl.GetAllBusStations();
+            ListLines.ItemsSource = bl.GetAllLines();
+        }
+
+        private void Exit(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UpdateStation_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateStation upS = new UpdateStation(bl);
+            upS.Closed += RefreshLinesAndStations;
+            upS.ShowDialog();
+        }
+
+        private void AddStation_Click(object sender, RoutedEventArgs e)
+        {
+            AddStation adds = new AddStation(bl);
+            adds.Closed += RefreshLinesAndStations;
+            adds.ShowDialog();
+        }
+        private void addline_Click(object sender, RoutedEventArgs e)
+        {
+            AddLine addl = new AddLine(bl);
+            addl.Closed += RefreshLinesAndStations;
+            addl.ShowDialog();
+        }
+
+        private void UpdateLine_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateLine upL = new UpdateLine(bl);
+            upL.Closed += RefreshLinesAndStations;
+            upL.ShowDialog();
+        }
+
+        private void DeleteBusStation_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void dataGridView1_CellClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DeleteLine_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ListLines_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            LineStations.ItemsSource = (ListLines.SelectedItem as BOLine).Stations;
+        }
+
+        private void ListBusStation_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            StationLines.ItemsSource = (ListBusStation.SelectedItem as BOBusStation).Lines;
+        }
+
     }
+}
 }
